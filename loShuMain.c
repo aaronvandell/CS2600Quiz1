8c0778a6f3a93411107d1406317b6b1f4ad80901
Aaron Van Dell <asvandell@cpp.edu>
2025-03-28 16:04:23 -0700

    Changed loShuMain.c to make it so it randomly generates squares until a Lo Shu Magic Square is generated

--- loShuMain.c
+++ loShuMain.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 typedef enum{
 	ls_success,
@@ -11,21 +12,65 @@ int main(void)
 	//defined in loShu.c
 	int loShuTest(int grid[3][3]);
 	
-	int loShuSquare[3][3] = {{9, 4, 2}, {3, 5, 7}, {8, 1, 6}};
+	int loShuSquare[3][3];
+	int counter = 0;
+	int result;
+	srand(time(NULL));
 
-	int result = loShuTest(loShuSquare);
-
-	if(result == ls_success)
+	while(result != ls_success)
 	{
-		printf("Success! The square is a Lo Shu Magic Square\n");
-		for(int i = 0; i < 3; i++)
+		//I know there is absolutely a better way to do it than this but I couldn't think of it.
+		loShuSquare[0][0] = (rand() % 9) + 1;
+		loShuSquare[0][1] = (rand() % 9) + 1;
+		while(loShuSquare[0][1] == loShuSquare[0][0])
 		{
-			printf("[%d %d %d]\n", loShuSquare[i][0], loShuSquare[i][1], loShuSquare[i][2]);
+			loShuSquare[0][1] = (rand() % 9) + 1;
 		}
+		loShuSquare[0][2] = (rand() % 9) + 1;
+		while(loShuSquare[0][2] == loShuSquare[0][0] || loShuSquare[0][2] == loShuSquare[0][1])
+		{
+			loShuSquare[0][2] = (rand() % 9) + 1;
+		}
+		loShuSquare[1][0] = (rand() % 9) + 1;
+		while(loShuSquare[1][0] == loShuSquare[0][0] || loShuSquare[1][0] == loShuSquare[0][1] || loShuSquare[1][0] == loShuSquare[0][2])
+		{
+			loShuSquare[1][0] = (rand() % 9) + 1;
+		}
+		loShuSquare[1][1] = (rand() % 9) + 1;
+		while(loShuSquare[1][1] == loShuSquare[0][0] || loShuSquare[1][1] == loShuSquare[0][1] || loShuSquare[1][1] == loShuSquare[0][2] || loShuSquare[1][1] == loShuSquare[1][0])
+		{
+			loShuSquare[1][1] = (rand() % 9) + 1;
+		}
+		loShuSquare[1][2] = (rand() % 9) + 1;
+		while(loShuSquare[1][2] == loShuSquare[0][0] || loShuSquare[1][2] == loShuSquare[0][1] || loShuSquare[1][2] == loShuSquare[0][2] || loShuSquare[1][2] == loShuSquare[1][0] || loShuSquare[1][2] == loShuSquare[1][1])
+		{
+			loShuSquare[1][2] = (rand() % 9) + 1;
+		}
+		loShuSquare[2][0] = (rand() % 9) + 1;
+		while(loShuSquare[2][0] == loShuSquare[0][0] || loShuSquare[2][0] == loShuSquare[0][1] || loShuSquare[2][0] == loShuSquare[0][2] || loShuSquare[2][0] == loShuSquare[1][0] || loShuSquare[2][0] == loShuSquare[1][1] || loShuSquare[2][0] == loShuSquare[1][2])
+		{
+			loShuSquare[2][0] = (rand() % 9) + 1;
+		}
+		loShuSquare[2][1] = (rand() % 9) + 1;
+		while(loShuSquare[2][1] == loShuSquare[0][0] || loShuSquare[2][1] == loShuSquare[0][1] || loShuSquare[2][1] == loShuSquare[0][2] || loShuSquare[2][1] == loShuSquare[1][0] || loShuSquare[2][1] == loShuSquare[1][1] || loShuSquare[2][1] == loShuSquare[1][2] || loShuSquare[2][1] == loShuSquare[2][0])
+		{
+			loShuSquare[2][1] = (rand() % 9) + 1;
+		}
+		loShuSquare[2][2] = (rand() % 9) + 1;
+		while(loShuSquare[2][2] == loShuSquare[0][0] || loShuSquare[2][2] == loShuSquare[0][1] || loShuSquare[2][2] == loShuSquare[0][2] || loShuSquare[2][2] == loShuSquare[1][0] || loShuSquare[2][2] == loShuSquare[1][1] || loShuSquare[2][2] == loShuSquare[1][2] || loShuSquare[2][2] == loShuSquare[2][0] || loShuSquare[2][2] == loShuSquare[2][1])
+		{
+			loShuSquare[2][2] = (rand() % 9) + 1;
+		}
+
+		counter++;
+		result = loShuTest(loShuSquare);
 	}
-	else
+	
+
+	printf("After %d attempts, found this successful Lo Shu Magic Square:\n", counter);
+	for(int i = 0; i < 3; i++)
 	{
-		printf("The square is not a Lo Shu Magic Square");
+		printf("[%d %d %d]\n", loShuSquare[i][0], loShuSquare[i][1], loShuSquare[i][2]);
 	}
 	
 }
